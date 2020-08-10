a64bba7953947b367ca53e6210e93dbf5f5601dc
Charlotte Gisele Weil <weilcharlotte@gmail.com>
2020-08-10 11:32:11 +0200

    viewerTemplate fixed

--- viewerTemplate/service-worker.js
+++ viewerTemplate/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/viewerTemplate/precache-manifest.2a4a879e1de6b1f623286b97aa80cd07.js"
+  "/viewerTemplate/precache-manifest.e20ac016933f0dbca5213208b8909e27.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/viewerTemplate/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
